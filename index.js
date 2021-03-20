3511f530c6586cd0b0a2d328d4b1f41a3ccbf701
Mohammadreza <mohammadreza.ha.va@gmail.com>
2021-03-20 12:17:33 +0330

    add style and change js and html

--- index.js
+++ index.js
@@ -6,50 +6,73 @@ const paper = document.querySelector('.paper');
 const hide = document.querySelector('.hide');
 const hhide = document.querySelector('.hhide');
 const message = document.querySelector('.message');
+const play = document.querySelector('.play');
+const contClick = document.querySelectorAll('.cont-click');
 const button = document.querySelector('button');
 const si = document.querySelector('.si');
 const el = document.querySelectorAll('.el');
 let userScore = 0;
 
-const elements = ['rock', 'paper', 'si'];
+const elements = ['rock', 'paper', 'scissors'];
 
-el.forEach(item => {
-	item.addEventListener('click', e => {
-		const chosen = e.target.innerText;
+contClick.forEach(item => {
+	item.addEventListener('click', () => {
+		const chosen = item.firstElementChild.id;
 		shit(chosen);
 	});
 });
 
 function shit(chosen) {
 	container.style.display = 'none';
-	hide.innerHTML = chosen;
-	const ran = Math.floor(Math.random() * 3);
+	show.style.display = 'flex';
+	hide.innerHTML = `
+		<div class="user-cont">
+			<img
+				src="img/icon-${chosen}.svg"
+				class="el ${chosen}"
+				id="${chosen}"
+			/>
+			<span class="white"></span>
+			<span class="bright bright-${chosen}"></span>
+			<span class="dark dark-${chosen}"></span>
+    	</div>
+	`;
+	const rand = () => Math.floor(Math.random() * 3);
+	const random = rand();
+	const ran = elements[random];
+	const shit = chosen === ran ? elements[(random + 1) % 3] : ran;
+
 	setTimeout(() => {
-		hhide.innerHTML = elements[ran];
+		hhide.innerHTML = `
+			<div class="com-cont">
+				<img src="img/icon-${shit}.svg" class="el ${shit}" id="${shit}" />
+				<span class="white"></span>
+				<span class="bright bright-${shit}"></span>
+				<span class="dark dark-${shit}"></span>
+			</div>
+		`;
 		setTimeout(() => {
-			win(chosen, elements[ran]);
+			win(chosen, shit);
 		}, 1000);
 	}, 1000);
 }
 
 function win(user, com) {
+	play.style.display = 'flex';
 	if (
-		(user === 'paper' && com === 'si') ||
+		(user === 'paper' && com === 'scissors') ||
 		(user === 'rock' && com === 'paper') ||
-		(user === 'si' && com === 'rock')
+		(user === 'scissors' && com === 'rock')
 	) {
-		message.innerHTML = 'computer won';
+		message.innerHTML = 'you lost';
 	}
 	if (
-		(user === 'si' && com === 'paper') ||
+		(user === 'scissors' && com === 'paper') ||
 		(user === 'paper' && com === 'rock') ||
-		(user === 'rock' && com === 'si')
+		(user === 'rock' && com === 'scissors')
 	) {
-		message.innerHTML = 'user won';
+		message.innerHTML = 'you won';
 		score.innerHTML = +score.innerHTML + 1;
-	}
-	if (user === com) {
-		message.innerHTML = 'equal';
 	}
 }
 
@@ -57,9 +80,7 @@ button.addEventListener('click', () => {
 	message.innerHTML = '';
 	hide.innerHTML = '';
 	hhide.innerHTML = '';
+	play.style.display = 'none';
+	show.style.display = 'none';
 	container.style.display = 'block';
 });
-
-setTimeout(() => {
-	console.log('hello');
-}, 2000);
